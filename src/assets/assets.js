973278e9b38502bc092ea151b3b94e363b7cd75b
shanto <shantohasibulhasan379@gmail.com>
2025-03-08 02:28:15 +0600

    commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -111,7 +111,7 @@ export const products = [{
 {
     _id: "aaaab",
     name: "Men Round Neck Pure Cotton T-shirt",
-    description: "A lightweight, usually knitted, pullover shirt, close-fitting and with",
+    description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves , worn as an undershirt or outer garment",
     price: 200,
     image: [p_img2_1,p_img2_2,p_img2_3,p_img2_4],
     category: "Men",
--- src/context/ShopeContext.jsx
+++ src/context/ShopeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from '../assets/assets'
 
 export const ShopeContext = createContext();
@@ -8,9 +8,37 @@ const ShopeContextProvider = (props) =>{
     const delevery_fee = 10
     const [search , setSearch] = useState('')
     const [showSearch , setShowSearch] = useState(false)
+    const [cartItem , setCartItem] = useState([])
+
+    const addToCart = async (itemId,size)=>{
+        let cartData = structuredClone(cartItem)
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] +=1
+            }  
+            else{
+                cartData[itemId][size] =1
+            }
+        }
+
+        else{
+            cartData[itemId] ={}
+            cartData[itemId][size] = 1
+        }
+        setCartItem(cartData)
+
+
+    }
+
+
+    useEffect(()=>{
+
+    },[])
+
     const value ={
         products,currency , delevery_fee,
-        search,setSearch,showSearch,setShowSearch 
+        search,setSearch,showSearch,setShowSearch ,
+        cartItem, addToCart
     }
     return(
         <ShopeContext.Provider value={value}>
